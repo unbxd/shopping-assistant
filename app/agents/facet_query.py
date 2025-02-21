0c3086e0110b7729e3634123c4a6a5984390080d
Rahul Vishwakarma <rahul.vishwakarma@unbxd.com>
2025-02-21 10:46:48 +0530

    feat: Improved facet question

--- app/agents/facet_query.py
+++ app/agents/facet_query.py
@@ -5,13 +5,13 @@ from config.config import OPENAI_API_KEY
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 
-def generate_filter_question(filter) -> str:
+def generate_filter_question(filter_options) -> str:
     response = client.chat.completions.create(
         model="gpt-4o-mini",  # Using 4o-mini model
         messages=[
             {"role": "system",
-             "content": f"Your task is to ask follow up question on the top facet in a friendly way. Give me only the query that should be asked to the user, so based on their response, I can apply filter and show the products. Do not give options in your query, I will be showing options to the user directly in the UI"},
-            {"role": "system", "content": json.dumps(filter, indent=4)},
+             "content": f"Your task is to ask follow up question on the top facet in a friendly way. Give me only the query that should be asked to the user, so based on their response, I can apply filter and show the products. **Do not give options in your query**, I will be showing options to the user directly in the UI. **Make sure to ask in super friendly way like a human, do not just give me a straight up blunt question. Don't use any greetings, as those are already sent at the start of the chat.**"},
+            {"role": "system", "content": json.dumps(filter_options, indent=4)},
         ],
     )
 
